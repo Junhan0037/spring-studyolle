24cca44cf6dccab3ec86840b3584b4bc1ea46b7e
Junhan0037 <wnsgks0037@naver.com>
2020-07-01 22:48:41 +0900

    ADD 회원 가입 컨트롤러

---
+++ src/test/java/com/studyolle/account/AccountControllerTest.java
@@ -0,0 +1,30 @@
+package com.studyolle.account;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AccountControllerTest {
+
+    @Autowired private MockMvc mockMvc;
+
+    @DisplayName("회원 가입 화면 보이는지 테스트")
+    @Test
+    void signUpForm() throws Exception {
+        mockMvc.perform(get("/sign-up"))
+                .andDo(print()) // view 내용 보기
+                .andExpect(status().isOk()) // 상태 확인
+                .andExpect(view().name("account/sign-up")); // view 파일 확인
+    }
+
+}
