c2dd0e3bc8e880f7f337831520d5fa564ae3086d
Junhan0037 <wnsgks0037@naver.com>
2020-07-04 18:53:40 +0900

    ADD 회원 가입 완료 후 자동 로그인

--- src/main/java/com/studyolle/account/AccountService.java
+++ src/main/java/com/studyolle/account/AccountService.java
@@ -4,12 +4,18 @@ import com.studyolle.domain.Account;
 import lombok.RequiredArgsConstructor;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 import javax.validation.Valid;
+import java.util.Collections;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -20,10 +26,11 @@ public class AccountService {
     private final PasswordEncoder passwordEncoder;
 
     @Transactional
-    public void processNewAccount(SignUpForm signUpForm) { // 작업 순서
+    public Account processNewAccount(SignUpForm signUpForm) { // 작업 순서
         Account newAccount = saveNewAccount(signUpForm); // 폼의 내용으로 회원 가입
         newAccount.generateEmailCheckToken(); // 이메일 확인하는 토큰 생성
         sendSignUpConfirmEmail(newAccount); // 가입 확인 이메일 보내기
+        return newAccount;
     }
 
     private Account saveNewAccount(@ModelAttribute @Valid SignUpForm signUpForm) {
@@ -44,6 +51,15 @@ public class AccountService {
         mailMessage.setSubject("스터디올래, 회원 가입 인증");
         mailMessage.setText("/check-email-token?token=" + newAccount.getEmailCheckToken() + "&email=" + newAccount.getEmail());
         javaMailSender.send(mailMessage);
+    }
+
+    public void login(Account account) {
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(
+                account.getNickname(),
+                account.getPassword(),
+                Collections.singleton(new SimpleGrantedAuthority("ROLE_USER")) // List.of(~)
+        );
+        SecurityContextHolder.getContext().setAuthentication(token);
     }
 
 }
