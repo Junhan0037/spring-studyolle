fefe3926eacc17a8409c73a6bbb22dc5639ca444
Junhan0037 <wnsgks0037@naver.com>
2020-07-06 18:03:10 +0900

    현재 인증된 사용자 정보 참조

---
+++ src/main/java/com/studyolle/account/UserAccount.java
@@ -0,0 +1,20 @@
+package com.studyolle.account;
+
+import com.studyolle.domain.Account;
+import lombok.Getter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Collections;
+
+@Getter
+public class UserAccount extends User {
+
+    private Account account;
+
+    public UserAccount(Account account) {
+        super(account.getNickname(), account.getPassword(), Collections.singleton(new SimpleGrantedAuthority("ROLE_USER")));
+        this.account = account;
+    }
+
+}
