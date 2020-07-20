21728a4615d50352c3bc1467caaa73500d422882
Junhan0037 <wnsgks0037@naver.com>
2020-07-20 16:58:51 +0900

    ADD 관심 주제 도메인

--- src/main/java/com/studyolle/domain/Account.java
+++ src/main/java/com/studyolle/domain/Account.java
@@ -4,6 +4,7 @@ import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.Set;
 import java.util.UUID;
 
 @Entity
@@ -53,6 +54,9 @@ public class Account { // 계정
     private boolean studyUpdatedByEmail;
 
     private boolean studyUpdatedByWeb = true;
+
+    @ManyToMany
+    private Set<Tag> tags;
 
     public void generateEmailCheckToken() { // 이메일 인증 토큰 생성
         this.emailCheckToken = UUID.randomUUID().toString();
