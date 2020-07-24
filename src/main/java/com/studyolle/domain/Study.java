ebe082cba4dcfa63188b6c0b1d8298ee1712391c
Junhan0037 <wnsgks0037@naver.com>
2020-07-24 21:06:49 +0900

    ADD 스터디 조회

--- src/main/java/com/studyolle/domain/Study.java
+++ src/main/java/com/studyolle/domain/Study.java
@@ -1,5 +1,6 @@
 package com.studyolle.domain;
 
+import com.studyolle.account.UserAccount;
 import lombok.*;
 
 import javax.persistence.*;
@@ -7,6 +8,11 @@ import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
+@NamedEntityGraph(name = "Study.withAll", attributeNodes = {
+        @NamedAttributeNode("tags"),
+        @NamedAttributeNode("zones"),
+        @NamedAttributeNode("managers"),
+        @NamedAttributeNode("members")}) // StudyRepository.findByPath() 호출 시 쿼리를 하나로 종합
 @Entity
 @Getter @Setter @EqualsAndHashCode(of = "id")
 @Builder @AllArgsConstructor @NoArgsConstructor
@@ -58,6 +64,19 @@ public class Study {
 
     public void addManager(Account account) {
         this.managers.add(account);
+    }
+
+    public boolean isJoinable(UserAccount userAccount) {
+        Account account = userAccount.getAccount();
+        return this.isPublished() && this.isRecruiting() && !this.members.contains(account) && !this.managers.contains(account);
+    }
+
+    public boolean isMember(UserAccount userAccount) {
+        return this.members.contains(userAccount.getAccount());
+    }
+
+    public boolean isManager(UserAccount userAccount) {
+        return this.managers.contains(userAccount.getAccount());
     }
 
 }
--- src/main/java/com/studyolle/study/StudyRepository.java
+++ src/main/java/com/studyolle/study/StudyRepository.java
@@ -1,10 +1,16 @@
 package com.studyolle.study;
 
 import com.studyolle.domain.Study;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional(readOnly = true)
 public interface StudyRepository extends JpaRepository<Study, Long> {
+
     boolean existsByPath(String path);
+
+    @EntityGraph(value = "Study.withAll", type = EntityGraph.EntityGraphType.LOAD) // 설정한 엔티티는 EAGER, 나머지는 LAZY 로딩
+    Study findByPath(String path);
+
 }
